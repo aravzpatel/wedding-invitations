aa6d63d2d5523f5bda56f10c00036bdb7c8ab547
Arav <arav.patel@gmail.com>
2020-08-14 16:24:43 +0100

    Create form with hard coded inputs

--- app/javascript/components/RsvpForm.js
+++ app/javascript/components/RsvpForm.js
@@ -1,12 +1,30 @@
 import React from "react"
 import PropTypes from "prop-types"
 class RsvPform extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { value: 'Grandma' }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({value: e.target.value })
+  }
+
+  handleSubmit(e){
+    alert('Form is working? ' + this.state.value );
+    e.preventDefault();
+  }
+
   render () {
     return (
       <div>
-        <h1>
-          I'm in the game
-        </h1>
+        <form onSubmit={this.props.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.props.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
