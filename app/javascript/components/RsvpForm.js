c23dae375707c06c208d6a85efac019192526308
Arav <arav.patel@gmail.com>
2020-08-14 12:11:23 +0100

    Create basic form component with console.log functionality

---
+++ app/javascript/components/RsvpForm.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class RsvPform extends React.Component {
+  render () {
+    console.log("Form is inputted")
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default RsvPform
