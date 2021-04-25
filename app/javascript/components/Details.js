5de96f03111919efd08e87331b9a5c33246697a7
Arav <arav.patel@gmail.com>
2021-04-25 18:18:53 +0100

    Added babel core

--- app/javascript/components/Details.js
+++ app/javascript/components/Details.js
@@ -4,7 +4,36 @@ const Details = () => {
   console.log("details get");
   return (
     <div>
-      <h1>11.30am start</h1>
+      <div>
+        <h1>Ceremony & Celebration</h1>
+        <p>The main event</p>
+        <p>Date: Saturday, 17th July</p>
+        <p>Venue: Master Shipwrights Palace</p>
+        <p>Address: Watergate St, Deptford, SE8 3JF</p>
+        <p>Arrive: 11.30 AM</p>
+        <p>End: 11 PM</p>
+        <p>Dress: Your best. Ditch the suit and come correct</p>
+      </div>
+      <div>
+        <h1>Hog Roast</h1>
+        <p>Wind down the next day at our local pub</p>
+        <p>Date: Sunday, 18th July</p>
+        <p>Venue: The Dog & Bell</p>
+        <p>Address: 116 Prince St, Deptford, SE8 3JD</p>
+        <p>Arrive: 1 PM</p>
+        <p>Dress: Chill</p>
+      </div>
+
+      <div>
+        <h1>Getting There</h1>
+        <p>
+          It's a family affair. We're keeping it local with both venues 5 mins
+          walk from our house
+        </p>
+        <p>Train: Deptford train station (10 mins walk)</p>
+        <p>Boat: Greenwich Pier (20 mins walk)</p>
+        <p>By Car: Free parking on Watergate Street</p>
+      </div>
     </div>
   );
 };
